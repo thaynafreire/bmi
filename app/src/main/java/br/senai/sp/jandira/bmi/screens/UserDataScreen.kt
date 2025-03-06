4cbdbbbf4f4872adb1c60bef117b5773b112805d
THAYNÁ DE OLIVEIRA FREIRE <24122484@dsn127m1247444.cfp127aluno.local>
2025-03-06 10:06:59 -0300

    box inicial criada, column e texto Hi também

--- app/src/main/java/br/senai/sp/jandira/bmi/screens/UserDataScreen.kt
+++ app/src/main/java/br/senai/sp/jandira/bmi/screens/UserDataScreen.kt
@@ -1,2 +1,56 @@
 package br.senai.sp.jandira.bmi.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import br.senai.sp.jandira.bmi.R
+
+@Composable
+fun UserDataScreen(modifier: Modifier = Modifier){
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                Color(0xffffffff)
+            )
+    ){
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.SpaceBetween,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = stringResource(
+                    R.string.hi_userdatascreen
+                ),
+                color = Color(0xff343434),
+                fontSize = 32.sp,
+                fontWeight = FontWeight.SemiBold
+
+            )
+        }
+    }
+}
+
+
+
+
+@Preview(showSystemUi = true)
+@Composable
+private fun UserDataScreenPreview(){
+    UserDataScreen()
+}
