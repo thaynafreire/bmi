7d6de94df1ed7bfd63325da8b956f416a5524b5e
THAYNÁ DE OLIVEIRA FREIRE <24122484@dsn127m1247444.cfp127aluno.local>
2025-03-13 09:46:01 -0300

    img

--- app/src/main/java/br/senai/sp/jandira/bmi/screens/UserDataScreen.kt
+++ app/src/main/java/br/senai/sp/jandira/bmi/screens/UserDataScreen.kt
@@ -1,19 +1,31 @@
 package br.senai.sp.jandira.bmi.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextForegroundStyle.Unspecified.brush
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import br.senai.sp.jandira.bmi.R
 
@@ -31,17 +43,58 @@ fun UserDataScreen(modifier: Modifier = Modifier){
             modifier = Modifier
                 .fillMaxSize(),
             verticalArrangement = Arrangement.SpaceBetween,
-            horizontalAlignment = Alignment.CenterHorizontally
+            horizontalAlignment = Alignment.Start
+
         ) {
             Text(
+                modifier = Modifier
+                    .padding(top = 90.dp)
+                    .padding(start = 35.dp),
                 text = stringResource(
                     R.string.hi_userdatascreen
                 ),
                 color = Color(0xff343434),
                 fontSize = 32.sp,
                 fontWeight = FontWeight.SemiBold
-
             )
+            Card (
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(700.dp),
+                shape = RoundedCornerShape(
+                    topStart = 40.dp,
+                    topEnd = 40.dp
+                ),
+                colors = CardDefaults
+                    .cardColors(
+                        containerColor = Color(0xffE6E6FA)
+                    )
+            ){
+                Column (
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ){
+                    Row (
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Start
+                    ){
+                        Column (
+                            modifier = Modifier,
+                            verticalArrangement = Arrangement.SpaceBetween
+                        ){
+                            Card (
+                                modifier = Modifier
+                                    .padding(50.dp),
+                            ){
+
+                            }
+
+                }
+                    }
+                }
+            }
         }
     }
 }
